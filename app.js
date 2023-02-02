c70cf77a60c279d735a2c1fe0dfc2d19cce72b94
Drew Lofland <alofland25@gmail.com>
2023-02-02 13:17:03 -0500

    finished variables exercise

--- app.js
+++ app.js
@@ -4,7 +4,30 @@ console.log(
 );
 
 // Exercise 1
-
+const firstName = "Drew"; 
+let lastName; 
+var age;
 // Exercise 2
-
+firstName = "Drew";
+lastName = "Lofland";
+age = 33;
 // Exercise 3
+let language = "JavaScript";
+let createdYeart = 1995;
+let isCaseSenitive = true;
+
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
+
+
+
